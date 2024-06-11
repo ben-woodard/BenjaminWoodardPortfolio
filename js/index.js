00157556a288f27a30abe09d6ea0b889e0735dbe
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-06-11 10:05:06 -0400

    changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
-imgElement = document.getElementById("github-img");
-introElement = document.getElementById("github-intro");
+const imgElement = document.getElementById("github-img");
+const introElement = document.getElementById("github-intro");
+const aboutMeButton = document.getElementById('about-me-button');
+const aboutMeBlurb = document.getElementById('about-me-blurb');
+const gitHubIntro = document.getElementById('github-intro');
+const resumeLinkContainer =  document.getElementById('resume-link-container');
+const open = document.getElementById('contact-modal-open');
+const modalContainer = document.getElementById('contact-modal-container');
+const send= document.getElementById('contact-modal-send');
+const close = document.getElementById('contact-modal-close')
+
 fetchGitHubUserInfo();
 
+
+
+
+//Fetching Info from Github
 async function fetchGitHubUserInfo() {
   try {
     const response = await fetch(`https://api.github.com/users/ben-woodard`);
@@ -29,3 +42,24 @@ async function fetchGitHubUserInfo() {
     console.error("Error fetching user info:", error);
   }
 }
+
+//About Me Section Visible
+aboutMeButton.addEventListener("click", () => {
+  aboutMeBlurb.classList.toggle('show');
+  gitHubIntro.classList.toggle('none');
+  resumeLinkContainer.classList.toggle('adjust');
+})
+
+//Contact Modal Container
+
+open.addEventListener('click', () => {
+  modalContainer.classList.add('show');
+});
+
+send.addEventListener('click', () => {
+  modalContainer.classList.remove('show');
+});
+
+close.addEventListener('click', () => {
+  modalContainer.classList.remove('show');
+});
